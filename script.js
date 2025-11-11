51db1148c2e394bed25dc2548ef6cab155fe4afe
DanielSiqueiraJackowski <danielfilho19990@gmail.com>
2025-11-11 11:05:20 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+const nome = "Daniel";
+let idade = 20;
+var cpf = 12121213331;
+
+document.getElementById('nome').textContent = nome;
+document.getElementById('idade').textContent = "idade:" + idade;
+
+function adicionar(){
+    idade++;
+    document.getElementById('idade').textContent = "idade:" + idade;
+
+}
+
+const typed = new Typed(".multiple-text", {
+    strings:
+    "eita"
+    "ele gooxxta",
+    "uuuuiii",
+    "caaavaaaaaaaalo",
+}
+
